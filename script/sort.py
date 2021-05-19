7339cf68875328f7a69ad94705c16b48bacdfb90
Dan Ziring <9089037+dzcode@users.noreply.github.com>
2021-05-19 17:17:39 -0500

    Readability changes to sorting script

--- script/sort.py
+++ script/sort.py
@@ -7,31 +7,31 @@ FILENAME = 'xwordlist.dict'
 def read():
     with open(FILENAME, 'r') as f:
         reader = csv.DictReader(f, fieldnames=['word', 'score'], delimiter=";")
-        d = []
-        error = False
+        parsed = []
+        has_error = False
         word_to_line = {}
         for index, row in enumerate([r for r in reader]):
-            # Ignore blank lines
-            if row['word'] is None:
+            word, score = row['word'], row['score']
+            if word is None:  # Ignore blank lines
                 continue
-            if row['score'] is None:
-                print(f"ERROR -- Missing score for {row['word']} on line {index + 1}")
-                error = True
+            if score is None:
+                print(f"has_error -- Missing score for {word} on line {index + 1}")
+                has_error = True
                 continue
-            row['score'] = int(row['score'].strip())
-            row['word'] = row['word'].upper().strip()
-            if row['word'] in word_to_line:
-                print(f"ERROR -- Word {row['word']} on lines ({word_to_line[row['word']]}, {index + 1})" \
+            score = int(score.strip())
+            word = word.upper().strip()
+            if word in word_to_line:
+                print(f"has_error -- Word {word} on lines ({word_to_line[word]}, {index + 1})" \
                        + " is in dictionary more than once")
-                error = True
+                has_error = True
                 continue
 
-            d.append(row)
-            word_to_line[row['word']] = index + 1
-        if error:
+            parsed.append({"word": word, "score": score})
+            word_to_line[word] = index + 1
+        if has_error:
             print("\n")
             raise Exception("See above errors")
-        return d
+        return parsed
 
 def sort(words):
     return sorted(words, key=lambda x: (-x['score'], x['word']))
@@ -46,8 +46,7 @@ def write(sorted_words):
 
 
 if __name__ == '__main__':
-    words = read()
-    words = sort(words)
+    words = sort(read())
 
     # Double-check the list is roughly as long as expected
     assert len(words) > 425000, f"Word list is too short ({len(words)} words), cancelling"
