5ad3e8072f0d01452e81b71d1eebafb9b5aa550f
crosswordnexus <crosswordnexus@gmail.com>
2021-05-19 20:33:47 -0700

    Update sort.py

--- script/sort.py
+++ script/sort.py
@@ -1,47 +1,40 @@
 #!/usr/bin/python3
 
-import csv
-
 FILENAME = 'xwordlist.dict'
 
 def read():
+    parsed = []
+    used_words = set()
     with open(FILENAME, 'r') as f:
-        reader = csv.DictReader(f, fieldnames=['word', 'score'], delimiter=";")
-        parsed = []
-        has_error = False
-        word_to_line = {}
-        for index, row in enumerate([r for r in reader]):
-            word, score = row['word'], row['score']
-            if word is None:  # Ignore blank lines
+        for line in f:
+            line = line.strip()
+            try:
+                word, score = line.split(';')
+            except:
                 continue
+            if not word:
+                continue
+            word = word.upper()
             if score is None:
-                print(f"has_error -- Missing score for {word} on line {index + 1}")
-                has_error = True
                 continue
             score = int(score.strip())
             word = word.upper().strip()
-            if word in word_to_line:
-                print(f"has_error -- Word {word} on lines ({word_to_line[word]}, {index + 1})" \
-                       + " is in dictionary more than once")
-                has_error = True
+            if word in used_words:
                 continue
-
+            else:
+                used_words.add(word)
+                
             parsed.append({"word": word, "score": score})
-            word_to_line[word] = index + 1
-        if has_error:
-            print("\n")
-            raise Exception("See above errors")
-        return parsed
+    return parsed
 
 def sort(words):
     return sorted(words, key=lambda x: x['word'])
 
 def write(sorted_words):
     with open(FILENAME, 'w') as f:
-        # Sort by score desc first, then alphabetically
-        writer = csv.DictWriter(f, fieldnames=['word', 'score'], delimiter=";")
         for w in sorted_words:
-            writer.writerow(w)
+            word, score = w['word'] ,w['score']
+            f.write(f'{word};{score}\n')
         print(f"Successfully sorted dictionary with {len(words)} words!")
 
 
