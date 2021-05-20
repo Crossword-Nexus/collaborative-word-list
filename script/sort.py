061ce7fdfc93df2373616f00cd1b96ba32021615
crosswordnexus <crosswordnexus@gmail.com>
2021-05-20 12:35:26 -0700

    Add more logic and comments to sort.py

--- script/sort.py
+++ script/sort.py
@@ -1,29 +1,53 @@
 #!/usr/bin/python3
 
+import unicodedata
+
 FILENAME = 'xwordlist.dict'
+
+def strip_accents(s):
+    """String accents from a string"""
+    return ''.join(c for c in unicodedata.normalize('NFD', s)
+                  if unicodedata.category(c) != 'Mn')
 
 def read():
     parsed = []
     used_words = set()
     with open(FILENAME, 'r') as f:
         for line in f:
+            # Remove leading and trailing whitespace
             line = line.strip()
+            # Split out the word and score
             try:
                 word, score = line.split(';')
             except:
                 continue
+            # Don't bother if we don't have a word
             if not word:
                 continue
+                
+            ## Normalize the word ##
+            # Strip extraneous whitespace
+            word = word.strip()
+            # Strip accents
+            word = strip_accents(word)
+            # Make uppercase
             word = word.upper()
+            # Remove any non-alphanumeric characters
+            word = ''.join(c for c in word if c.isalnum())
+            
+            # Don't bother if we don't have a score
             if score is None:
                 continue
+            # Cast score as an int
             score = int(score.strip())
-            word = word.upper().strip()
+            
+            # Don't use words more than once
             if word in used_words:
                 continue
             else:
                 used_words.add(word)
-                
+            
+            # Add this word to our collection 
             parsed.append({"word": word, "score": score})
     return parsed
 
